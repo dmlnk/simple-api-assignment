b90d0b34f4f84f98e46cb62c1fcb5e8bb0e7b995
Daniel Miller <daniel.miller.prs@gmail.com>
2021-05-14 14:54:44 +0200

    init: initial program structure added

---
+++ main.py
@@ -0,0 +1,33 @@
+KANYE_WEST_API_LINK = "https://api.kanye.rest/"
+SENTIM_API_LINK = "https://sentim-api.herokuapp.com/api/v1/"
+SENTIM_PARAMS = {"Accept": "application/json", "Content-Type": "application/json"}
+MIN_QUOTES_NUMBER = 5
+MAX_QUOTES_NUMBER = 20
+
+def get_kw_quote():
+  pass
+
+
+def get_user_input():
+  pass
+
+
+def collect_kw_quotes(number):
+  pass
+  
+
+def get_sentiment(sentence):
+  pass
+
+
+def separate_sentiment(quotes):
+  pass
+  
+
+def display_result(sentiment_dict):
+  pass
+
+
+if __name__ == "__main__":
+  pass
+    
