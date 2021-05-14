bb1af964c7e75611e1b4ee4f339c16e4d478f353
Daniel Miller <daniel.miller.prs@gmail.com>
2021-05-14 15:01:09 +0200

    feat: Kanye West API call implemented

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import requests
+import json
+import sys
+
 KANYE_WEST_API_LINK = "https://api.kanye.rest/"
 SENTIM_API_LINK = "https://sentim-api.herokuapp.com/api/v1/"
 SENTIM_PARAMS = {"Accept": "application/json", "Content-Type": "application/json"}
@@ -5,7 +9,34 @@ MIN_QUOTES_NUMBER = 5
 MAX_QUOTES_NUMBER = 20
 
 def get_kw_quote():
-  pass
+  try:
+    response = requests.get(KANYE_WEST_API_LINK)
+  except:
+    print("Connection error.")
+    sys.exit(-1)
+
+  data = json.loads(response.text)
+  quote_str = data["quote"]
+  status = response.status_code
+
+  # basic error checking - there are way more errors tbh:()
+  if status == 301:
+    print("The URL of the requested resource has been changed permanently.")
+    sys.exit(-1)
+  elif status == 400:
+    print("The server could not understand the request due to invalid syntax.")
+    sys.exit(-1)
+  elif status == 403:
+    print("The client does not have access rights to the content.")
+    sys.exit(-1)
+  elif status == 404:
+    print("Requested resource cannot be found.")
+    sys.exit(-1)
+  elif status == 503:
+    print("The server is not ready to handle the request.")
+    sys.exit(-1)
+
+  return quote_str
 
 
 def get_user_input():
